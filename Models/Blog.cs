7271159609c005d0717c023299f384b82b72d2d8
Mark Donile <donile@gmail.com>
2018-08-14 21:45:00 -0500

    Add BlogPost class

---
+++ Models/Blog.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace markdonile.com
+{
+    public class BlogPost
+    {
+        public string Post { get; }
+        public DateTime WrittenDate { get; }
+        public DateTime ReleaseDate { get; }
+        public String Author { get; }
+        public String Title { get; }
+    }
+}
