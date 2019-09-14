4338891124d3f4f8426976a57a11d30bc3763d00
Mark Donile <donile@gmail.com>
2019-09-14 08:09:03 -0500

    trello-27: Fix relative paths

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,18 +2,18 @@ echo "Start deploying ..."
 
 echo "Start publishing .NET Core project ..."
 
-dotnet publish ../src/Blog/Blog.csproj --configuration Release
+dotnet publish ./src/Blog/Blog.csproj --configuration Release
 
 echo "Finished publishing .NET Core project ..."
 
 echo "Start deploying .NET Core project ..."
 
-scp -i "$DEPLOYER_SSH_PRIVATE_KEY" -r ../artifacts/bin/Release/Blog/netcoreapp2.2/publish $DEPLOYER_USERNAME@markdonile.com:/home/$DEPLOYER_USERNAME/markdonile.com
+scp -i "$DEPLOYER_SSH_PRIVATE_KEY" -r ./artifacts/bin/Release/Blog/netcoreapp2.2/publish $DEPLOYER_USERNAME@markdonile.com:/home/$DEPLOYER_USERNAME/markdonile.com
 
 ssh -l $DEPLOYER_USERNAME -i "$DEPLOYER_SSH_PRIVATE_KEY" markdonile.com << HERE
     sudo systemctl stop kestrel.markdonile.com.service
     rm -r /var/www/markdonile.com/*
-    cp -a /home/$DEPLOYER_USERNAME/markdonile.com/. /var/www/markdonile.com/
+    cp -a /home/deployer/markdonile.com/. /var/www/markdonile.com/
     sudo systemctl start kestrel.markdonile.com.service
     logout
 HERE
