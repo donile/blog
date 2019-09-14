892dcbcec24f90e85552b02f00cf29f818a18b80
Mark Donile <donile@gmail.com>
2019-09-14 08:17:25 -0500

    trello-27: Prevent dotnet restore and build during publish

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,7 +2,7 @@ echo "Start deploying ..."
 
 echo "Start publishing .NET Core project ..."
 
-dotnet publish ./src/Blog/Blog.csproj --configuration Release
+dotnet publish ./src/Blog/Blog.csproj --no-restore --no-build --configuration Release
 
 echo "Finished publishing .NET Core project ..."
 
@@ -13,7 +13,7 @@ scp -i "$DEPLOYER_SSH_PRIVATE_KEY" -r ./artifacts/bin/Release/Blog/netcoreapp2.2
 ssh -l $DEPLOYER_USERNAME -i "$DEPLOYER_SSH_PRIVATE_KEY" markdonile.com << HERE
     sudo systemctl stop kestrel.markdonile.com.service
     rm -r /var/www/markdonile.com/*
-    cp -a /home/deployer/markdonile.com/. /var/www/markdonile.com/
+    cp -r /home/deployer/markdonile.com/. /var/www/markdonile.com/
     sudo systemctl start kestrel.markdonile.com.service
     logout
 HERE
