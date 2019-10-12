0c16d9cb22d049fe6e47fa73c804fea2b3ab63a4
Mark Donile <donile@gmail.com>
2019-10-12 15:03:55 -0500

    trello-26: Display BlogPost in HomePage

--- src/Blog/ClientApp/src/components/HomePage.vue
+++ src/Blog/ClientApp/src/components/HomePage.vue
@@ -2,14 +2,19 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-4 offset-4">
-                <p class="text-center">Future home of BlogPost.Vue</p>
+                <blog-post/>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import BlogPost from './BlogPost';
+
 export default {
     name: 'HomePage',
+    components: {
+        'blog-post': BlogPost
+    }
 }
 </script>
