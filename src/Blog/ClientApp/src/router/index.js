a46daa0992d813dc730bee048a8332d213175042
Mark Donile <donile@gmail.com>
2019-10-13 12:20:55 -0500

    trello-33: Enable HTML5 history mode

--- src/Blog/ClientApp/src/router/index.js
+++ src/Blog/ClientApp/src/router/index.js
@@ -7,8 +7,10 @@ import AdminHome from '../components/AdminHome';
 Vue.use(VueRouter);
 
 export default new VueRouter({
+    mode: "history",
     routes: [
         { path: "/", component: HomePage },
-        { path: "/admin", component: AdminHome }
+        { path: "/admin", component: AdminHome },
+        { path: "*", redirect: "/" }
     ]
 })
