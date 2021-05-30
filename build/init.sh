0a43417c1ab75c9fd2321b080832a3c8134efb0d
Mark Donile <donile@gmail.com>
2021-05-30 08:28:12 -0500

    Initialize database with psql script

--- build/init.sh
+++ build/init.sh
@@ -1,4 +1,6 @@
 psql \
     postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432 \
-    -variable password="'${POSTGRES_BLOG_PASSWORD}'" \
+    --variable password="'${POSTGRES_BLOG_PASSWORD}'" \
+    --variable role="${POSTGRES_BLOG_USER}" \
+    --variable database="${POSTGRES_BLOG_DATABASE}" \
     --file src/Blog.Database/init/init.sql
